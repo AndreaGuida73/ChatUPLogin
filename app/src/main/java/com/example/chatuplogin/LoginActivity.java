389efba98b3783198126735e44bcfb2bc0698d03
AndreaGuida73 <andrea.guida@email.it>
2020-05-10 12:24:18 +0200

    23

--- app/src/main/java/com/example/chatuplogin/LoginActivity.java
+++ app/src/main/java/com/example/chatuplogin/LoginActivity.java
@@ -1,13 +1,60 @@
 package com.example.chatuplogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    final String mUtente = "Andrea";
+    final String mPwd = "1234";
+
+    EditText nomeUtente;
+    EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        nomeUtente = (EditText) findViewById(R.id.etRegNome);
+        password = (EditText) findViewById(R.id.etRegPassword);
+
+
     }
+
+
+
+    public void tvRegisgtratiClick(View view) {
+        Log.d("Chat","Tasto Registrami premuto");
+        Intent intent1 = new Intent(this, RegisterActivity.class);
+
+        startActivity(intent1);
+    }
+
+
+
+        public void btnLoginClick(View view) {
+            Log.d("Chat","Tasto Login premuto");
+
+            if(mUtente.equals(nomeUtente.getText().toString()) && mPwd.equals(password.getText().toString())){
+
+                Intent intent2 = new Intent(this, MainActivity.class);
+                intent2.putExtra("messaggio",nomeUtente.getText().toString());
+                startActivity(intent2);
+
+            } else {
+
+                Toast.makeText(this,"Credenziali Errate",Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+
+
+
 }
--- app/src/main/java/com/example/chatuplogin/RegisterActivity.java
+++ app/src/main/java/com/example/chatuplogin/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.chatuplogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void tvLoginClick(View view) {
+        Log.d("Chat","e' stato premuto il tasto Login");
+        Intent intent1 = new Intent(this, LoginActivity.class);
+        startActivity(intent1);
+    }
+
+    public void btnRegisterClick(View view) {
+        Log.d("Chat","e' stato premuto il tasto Register");
     }
 }
